b35899f350412cdf957a2601b76e6524739d9692
TiesWestendorp <tieswestendorp@gmail.com>
2025-06-18 13:42:19 +0200

    Insert into main.py

--- cutting-stock/main.py
+++ cutting-stock/main.py
@@ -16,10 +16,35 @@ def patterns(finals: list[int], raw_length: int) -> Iterator[dict[int,int]]:
             yield dict(zipped)
 
 def cutting_stock(raw_length: int, final_demands: dict[int,int]) -> list[tuple[dict[int,int],int]]:
-    finals = final_demands.keys()
+    finals = list(final_demands.keys())
     possible_patterns = list(patterns(finals, raw_length))
 
-    raise NotImplementedError # TODO: Optimize using `linprog`
+    M = sum(final_demands.values())
+    over_production_per_final = [0.5 * f for f in finals]
+    waste_per_pattern = [raw_length - sum(final * count for final,count in pattern.items()) for pattern in possible_patterns]
+    zeroes = [0 for _ in possible_patterns]
+    ones = [1 for _ in possible_patterns]
+    A_eq = []
+    b_eq = []
+    for final in finals:
+        A_eq.append([-pattern[final] for pattern in possible_patterns] + [f == final for f in finals] + zeroes)
+        b_eq.append(-final_demands[final])
+    A_ub = []
+    b_ub = []
+    for i,_ in enumerate(possible_patterns):
+        A_ub.append([i == j for j,_ in enumerate(possible_patterns)] + [0 for _ in finals] + [-M * (i == j) for j,_ in enumerate(possible_patterns)])
+        b_ub.append(0)
+    A_ub.append(zeroes + [0 for _ in finals] + ones)
+    b_ub.append(3)
+
+    result = linprog(
+        waste_per_pattern + over_production_per_final + zeroes,
+        A_eq = A_eq, b_eq = b_eq,
+        A_ub = A_ub, b_ub = b_ub,
+        integrality = ones + [0 for _ in finals] + ones,
+        bounds = (len(possible_patterns)+len(finals))*[(0,None)] + len(possible_patterns)*[(0,1)])
+
+    return list(filter(lambda x: x[1] > 0, zip(possible_patterns, map(int, result.x))))
 
 if __name__ == "__main__":
     import argparse
@@ -27,37 +52,37 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Cutting stock problem solver")
     parser.add_argument("--file", required=True, help="CSV input file")
     file_name = parser.parse_args().file
- 
+
     raw_length = None
     final_demands = None
     with open(file_name, "r") as file:
         lines = file.readlines()
         raw_length = int(lines[0])
         final_demands = dict(map(lambda line: map(int, line.split(",")), lines[1:]))
-    
+
     print(f"Specified raw length: {raw_length}")
     print(f"Specified final demands: {final_demands}\n")
-    
+
     result = cutting_stock(raw_length, final_demands)
 
     print("Result")
     print("------")
-    print(f"Raws used: {sum(count for _, count in result)}\n")
+    print(f"Raws used: {sum(count for _,count in result)}\n")
 
     for pattern,count in result:
         # We don't show unused patterns
         if count == 0:
             continue
-        
-        pattern_without_zero_entries = {final: amount for final, amount in pattern.items() if amount>0}
+
+        pattern_without_zero_entries = {final: amount for final,amount in pattern.items() if amount>0}
         print(f"{count} times:\t{pattern_without_zero_entries}")
-    
-    waste = sum(count * (raw_length - sum(final * amount for final, amount in pattern.items())) for pattern, count in result)
+
+    waste = sum(count * (raw_length - sum(final*amount for final,amount in pattern.items())) for pattern,count in result)
     print(f"\nTotal produced waste: {waste}\n")
 
     for final in final_demands:
         # We don't show finals that aren't overproduced
-        produced = sum(count * pattern[final] for pattern, count in result)
+        produced = sum(count * pattern[final] for pattern,count in result)
         overproduced_amount = produced-final_demands[final]
         if overproduced_amount <= 0:
             continue
