3f34c3c26f01950cc6ef87e92195232415ac5d33
TiesWestendorp <tieswestendorp@gmail.com>
2025-06-20 10:19:06 +0200

    Minor changes

--- cutting-stock/main.py
+++ cutting-stock/main.py
@@ -16,14 +16,19 @@ def patterns(finals: list[int], raw_length: int) -> Iterator[Pattern]:
         waste = raw_length - pattern_length
 
         # The pattern must not exceed the raw_length, and no final must be cuttable from the waste
-        if waste >= 0 and waste <= smallest_final:
+        if 0 <= waste <= smallest_final:
             yield dict(zipped)
 
 def cutting_stock(raw_length: int, final_demands: dict[int,int]) -> list[tuple[Pattern,int]]:
-    finals = final_demands.keys()
+    finals = list(final_demands.keys())
     possible_patterns = list(patterns(finals, raw_length))
 
-    raise NotImplementedError # TODO: Optimize using `linprog`
+    raise NotImplementedError
+
+    # TODO: Optimize using `linprog`
+    result = linprog()
+
+    return list(zip(possible_patterns, map(int, result.x)))
 
 if __name__ == "__main__":
     import argparse
@@ -54,7 +59,7 @@ if __name__ == "__main__":
             continue
         pattern_without_zero_entries = {final: amount for final,amount in pattern.items() if amount>0}
         print(f"{count} times:\t{pattern_without_zero_entries}")
-    
+
     waste = sum(count * (raw_length - sum(final*amount for final,amount in pattern.items())) for pattern,count in result)
     print(f"\nTotal produced waste: {waste}\n")
 
